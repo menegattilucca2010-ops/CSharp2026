ffc3aa9252ca8abbdceb1fbe486c11ed039edc55
menegattilucca2010 <menegattilucca2010@gmail.com>
2026-02-13 20:11:26 -0300

    Aula03

---
+++ Lógica de Programação e Algoritimos/Aula3/NaoSeiNada.cs
@@ -0,0 +1,10 @@
+double x , y , media;
+Console.WriteLine("Digite o primeiro numero:");
+x = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo numero:");
+y = double.Parse(Console.ReadLine());
+media = (x + y) / 2;
+Console.WriteLine("A media é: " + media);
+
+
+
